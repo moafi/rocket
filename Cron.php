4d008f389c22ff30a0770282ca061da1d3dc1479
moafi <moafi.mahdi@yahoo.com>
2021-09-01 14:30:46 +0430

    Update Cron.php

--- Cron.php
+++ Cron.php
@@ -3,6 +3,8 @@ function rocket_rrmdir($dir, $dirs_to_preserve = array())
 {
     $dir1 = $dir . '/wp-content/cache/wp-rocket';
     $dir2 = $dir . '/wp-content/cache/min';
+    $dir3 = $dir . '/wp-content/cache/busting';
+
     if (!is_dir($dir1))
     {
         @unlink($dir1);
@@ -11,7 +13,10 @@ function rocket_rrmdir($dir, $dirs_to_preserve = array())
     if (!is_dir($dir2))
     {
         @unlink($dir2);
-        return;
+    }
+    if (!is_dir($dir3))
+    {
+        @unlink($dir3);
     }
     if ($dirs = glob($dir . '/*', GLOB_NOSORT))
     {
@@ -28,6 +33,3 @@ function rocket_rrmdir($dir, $dirs_to_preserve = array())
         }
     }
 }
-
-rocket_rrmdir('');
-
